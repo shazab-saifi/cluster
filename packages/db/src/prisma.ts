983e6e49e7b3bc4225fce0821a68820d218d8442
shazab-saifi <shazebsaifi92@gmail.com>
2026-04-28 21:28:15 +0530

    Created prisma schema for network, channel, and message

--- packages/db/src/prisma.ts
+++ packages/db/src/prisma.ts
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 (async () => {
   try {
     await prisma.$connect();
-    console.log("Connect to database successfully!");
+    console.log("Connected to database successfully!");
   } catch (error) {
     console.error("Failed to connect to database:", error);
   }
