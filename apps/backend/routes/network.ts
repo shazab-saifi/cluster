44f373bc3d65409d4f1726b79301dac3ed02f8e5
shazab-saifi <shazebsaifi92@gmail.com>
2026-04-30 20:37:24 +0530

    Created network search endpoint

--- apps/backend/routes/network.ts
+++ apps/backend/routes/network.ts
@@ -44,3 +44,29 @@ networkRouter.post("/", async (req: Request, res: Response) => {
       .json({ error: "Internal server error, Please try again later!" });
   }
 });
+
+networkRouter.get("/search", async (req: Request, res: Response) => {
+  const query = req.query.q;
+
+  if (!query) {
+    return res
+      .status(400)
+      .json({ error: "No 'q' query was provided in the URI!" });
+  }
+
+  try {
+    const searcheResult = await prisma.$queryRaw`
+        SELECT *, similarity(name, ${query}) AS score
+        FROM "network"
+        WHERE similarity(name, ${query}) > 0.1
+        ORDER BY score DESC;
+      `;
+
+    res.json(searcheResult);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error, Please try again later!" });
+  }
+});
