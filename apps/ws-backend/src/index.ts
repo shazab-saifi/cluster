777d5a8de2ce1e56af906b3a5cb08dc42536fe77
shazab-saifi <shazebsaifi92@gmail.com>
2026-05-08 17:42:40 +0530

    Created create, update, delete message services

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data, isBinary) => {});
+});
--- packages/core/services/messages-servines.ts
+++ packages/core/services/messages-servines.ts
@@ -1,8 +1,26 @@
-import { Message } from "@workspace/db";
+import { prisma } from "@workspace/db";
 
 export async function createMessage(
-  networkId: string,
   channelId: string,
   userId: string,
-  data: Message
-) {}
+  message: string
+) {
+  return await prisma.message.create({
+    data: {
+      channelId,
+      senderId: userId,
+      message,
+    },
+  });
+}
+
+export async function updateMessage(messageId: string, message: string) {
+  return await prisma.message.update({
+    where: { id: messageId },
+    data: { message },
+  });
+}
+
+export async function deleteMessage(messageId: string) {
+  return await prisma.message.delete({ where: { id: messageId } });
+}
