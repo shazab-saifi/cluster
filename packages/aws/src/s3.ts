7cd9aeb53f612580de1b75742468341ce0ada661
shazab-saifi <shazebsaifi92@gmail.com>
2026-06-13 21:47:43 +0530

    Created an aws internal package, probably will need to add more aws services in future that's why created an package

---
+++ packages/aws/src/s3.ts
@@ -0,0 +1,6 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+// NOTE: aws will use local machine credentials if not provided explicitly in configuration. I've add an IAM in my machine
+export const s3Client = new S3Client({});
+
+export * from "@aws-sdk/client-s3";
