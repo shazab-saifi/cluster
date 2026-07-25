d0e84bff10b1505f904c517c2b69d446a1ed302d
shazab-saifi <shazebsaifi92@gmail.com>
2026-07-25 20:50:22 +0530

    Created edit-input.tsx which takes message place when user wants to edit a message

---
+++ apps/web/components/dashboard/chat-section/edit-input.tsx
@@ -0,0 +1,80 @@
+import { SmileyIcon, XIcon } from "@phosphor-icons/react";
+import { Button } from "@workspace/ui/components/button";
+import { useLayoutEffect, useRef, useState } from "react";
+
+interface EditInputProps {
+  message: string;
+  messageId: string;
+  handleEdit: (messageId: string, message: string) => void;
+  setIsEditing: (value: { messageId: string; message: string } | null) => void;
+}
+
+const EditInput = ({
+  message,
+  messageId,
+  handleEdit,
+  setIsEditing,
+}: EditInputProps) => {
+  const [editedMessage, setEditedMessage] = useState<string>(message);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useLayoutEffect(() => {
+    const input = inputRef.current;
+    if (!input) return;
+
+    input.focus();
+    const len = input.value.length;
+    input.setSelectionRange(len, len);
+  }, [messageId]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && editedMessage.trim().length > 0) {
+      handleEdit(messageId, editedMessage);
+    } else if (e.key === "Escape") {
+      setIsEditing(null);
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-1 items-center justify-between rounded-lg border-2 bg-muted p-2">
+        <input
+          ref={inputRef}
+          name="message-edit-input"
+          autoFocus
+          value={editedMessage}
+          onChange={(e) => setEditedMessage(e.target.value)}
+          className="bg-transparen flex-1 px-1 focus:outline-none"
+          onKeyDown={handleKeyDown}
+        />
+        <div className="flex gap-1">
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon-lg"
+            aria-label="Choose emoji"
+            className="text-muted-foreground hover:text-foreground"
+          >
+            <SmileyIcon className="size-5" />
+          </Button>
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon-lg"
+            aria-label="Cancel edit"
+            className="text-muted-foreground hover:text-foreground"
+            onClick={() => setIsEditing(null)}
+          >
+            <XIcon />
+          </Button>
+        </div>
+      </div>
+      <p className="mt-1 text-xs">
+        Press <span className="text-accent">ESC</span> key to cancel edit, or{" "}
+        <span className="text-accent">Enter</span> key to complete edit.
+      </p>
+    </div>
+  );
+};
+
+export default EditInput;
